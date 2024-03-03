e1d90e2f176af0ffdab6fd8b78a4317d345c7d53
DaveedGangi <daveedgangi@gmail.com>
2024-03-03 14:38:48 +0000

    SimpleTodos

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,48 +1,66 @@
 import {Component} from 'react'
 
+import {v4 as uuid} from 'uuid'
+
 import TodoItemPart from '../TodoItem'
 
 import './index.css'
 
 const initialTodosList = [
   {
-    id: 1,
+    id: uuid(),
     title: 'Book the ticket for today evening',
+    Edit: false,
+    checking: false,
   },
   {
-    id: 2,
+    id: uuid(),
     title: 'Rent the movie for tomorrow movie night',
+    Edit: false,
+    checking: false,
   },
   {
-    id: 3,
+    id: uuid(),
     title: 'Confirm the slot for the yoga session tomorrow morning',
+    Edit: false,
+    checking: false,
   },
   {
-    id: 4,
+    id: uuid(),
     title: 'Drop the parcel at Bloomingdale',
+    Edit: false,
+    checking: false,
   },
   {
-    id: 5,
+    id: uuid(),
     title: 'Order fruits on Big Basket',
+    Edit: false,
+    checking: false,
   },
   {
-    id: 6,
+    id: uuid(),
     title: 'Fix the production issue',
+    Edit: false,
+    checking: false,
   },
   {
-    id: 7,
+    id: uuid(),
     title: 'Confirm my slot for Saturday Night',
+    Edit: false,
+    checking: false,
   },
   {
-    id: 8,
+    id: uuid(),
     title: 'Get essentials for Sunday car wash',
+    Edit: false,
+    checking: false,
   },
 ]
 
 // Write your code here
 
 class SimpleTodos extends Component {
-  state = {todo: initialTodosList}
+  state = {todo: initialTodosList, userText: ''}
 
   deleteCard = id => {
     console.log(id)
@@ -51,18 +69,94 @@ class SimpleTodos extends Component {
     this.setState({todo: removes})
   }
 
+  userValue = event => {
+    this.setState({userText: event.target.value})
+  }
+
+  addUserInput = () => {
+    const {userText} = this.state
+
+    const newTodo = {
+      id: uuid(),
+      title: userText,
+      Edit: false,
+      checking: false,
+    }
+
+    this.setState(prevState => ({todo: [...prevState.todo, newTodo]}))
+    this.setState({userText: ''})
+  }
+
+  changeToEdit = id => {
+    const {userText} = this.state
+    console.log(id)
+    this.setState(prevState => ({
+      todo: prevState.todo.map(each => {
+        if (id === each.id) {
+          return {...each, Edit: !each.Edit, title: userText}
+        }
+        return each
+      }),
+    }))
+  }
+
+  changeToSave = id => {
+    console.log(id)
+    this.setState(prevState => ({
+      todo: prevState.todo.map(each => {
+        if (id === each.id) {
+          return {...each, Edit: !each.Edit}
+        }
+        return each
+      }),
+    }))
+  }
+
+  changeToStyle = id => {
+    this.setState(prevState => ({
+      todo: prevState.todo.map(each => {
+        if (id === each.id) {
+          return {...each, checking: !each.checking}
+        }
+        return each
+      }),
+    }))
+  }
+
   render() {
-    const {todo} = this.state
+    const {todo, userText} = this.state
     return (
       <div className="bg">
         <div className="card">
           <h1 className="heading">Simple Todos</h1>
-          <ul>
+
+          <div className="leftSide">
+            <div className="addWidth">
+              <div>
+                <input value={userText} onChange={this.userValue} type="text" />
+              </div>
+              <div>
+                <button
+                  className="Add"
+                  type="button"
+                  onClick={this.addUserInput}
+                >
+                  Add
+                </button>
+              </div>
+            </div>
+          </div>
+          <ul className="ul">
             {todo.map(each => (
               <TodoItemPart
                 todoItem={each}
                 key={each.id}
                 removingCard={this.deleteCard}
+                edit={each.Edit}
+                changeToEditTodo={this.changeToEdit}
+                changeToSaveTodo={this.changeToSave}
+                editUserValue={this.userValue}
+                changeStyles={this.changeToStyle}
               />
             ))}
           </ul>
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,23 +1,76 @@
 // Write your code here
 
+import {useState} from 'react'
+
 import './index.css'
 
 const TodoItemPart = props => {
-  const {todoItem, removingCard} = props
+  const {
+    todoItem,
+    removingCard,
+    changeToEditTodo,
+    changeToSaveTodo,
+    edit,
+    changeStyles,
+  } = props
 
-  const {title, id} = todoItem
+  const {title, id, checking} = todoItem
+
+  const [textField, setTextField] = useState(title)
 
   const removeCard = () => {
     removingCard(id)
     console.log(id)
   }
 
+  const changeToSaves = () => {
+    changeToSaveTodo(id)
+  }
+
+  const changeToEdits = () => {
+    changeToEditTodo(id)
+  }
+
+  const userValueChange = event => {
+    setTextField(event.target.value)
+  }
+
+  const changeStyling = () => {
+    changeStyles(id)
+  }
+
   return (
     <li className="EachTodo">
-      <p className="eachTodo">{title}</p>
-      <button className="deleteButton" onClick={removeCard} type="button">
-        Delete
-      </button>
+      <input type="checkbox" onClick={changeStyling} />
+      {edit ? (
+        <input
+          className="inputUser"
+          onChange={userValueChange}
+          type="text"
+          value={textField}
+        />
+      ) : (
+        <p className={checking ? 'textDecorate' : 'eachTodo'}>{textField}</p>
+      )}
+
+      <div className="ForButton">
+        <div>
+          <button className="deleteButton" onClick={removeCard} type="button">
+            Delete
+          </button>
+        </div>
+        <div>
+          {edit ? (
+            <button className="Save" type="button" onClick={changeToEdits}>
+              Save
+            </button>
+          ) : (
+            <button className="Edit" type="button" onClick={changeToSaves}>
+              Edit
+            </button>
+          )}
+        </div>
+      </div>
     </li>
   )
 }
